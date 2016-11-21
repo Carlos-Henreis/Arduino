b51faf61b8abf14fe932c5e590159f32e44703b1
Carlos Henrique Reis <carlos_henreis@unifei.edu.br>
2016-11-21 00:50:06 -0200

    Programas de simples

---
+++ ToyProgram/piscaLed.ino
@@ -0,0 +1,14 @@
+/* Projeto arduino pisca led
+ * Autor Carlos
+*/
+ 
+void setup() {
+  pinMode(10,OUTPUT);
+}
+ 
+void loop() {
+  digitalWrite(10,HIGH);
+  delay(1000);
+  digitalWrite(10,LOW);
+  delay(1000);
+}
